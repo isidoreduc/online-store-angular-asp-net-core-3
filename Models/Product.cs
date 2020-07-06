6c1790a1f76f6b84c2f9d9e094bb0d0640e943d1
isidoreduc <isidoreducasse79@gmail.com>
2020-07-06 19:10:43 +0200

    database connection

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,7 +9,7 @@ namespace ServerApp.Models
             public string Name { get; set; }
             public string Description { get; set; }
             public string Category { get; set; }
-            [Column(TypeName = "decimal(8,2")]
+            [Column(TypeName = "decimal(8,2)")]
             public decimal Price { get; set; }
             public Supplier Supplier { get; set; }
             public List<Rating> Ratings { get; set; }
