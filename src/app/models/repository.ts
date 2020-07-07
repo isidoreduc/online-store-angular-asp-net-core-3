207e3aea9fcf29ad138feb0a6e1a1e2670e1f27c
isidoreduc <isidoreducasse79@gmail.com>
2020-07-07 12:57:04 +0200

    getting data from API

--- src/app/models/repository.ts
+++ src/app/models/repository.ts
@@ -1,9 +1,22 @@
 import { Product } from './product.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+@Injectable()
 export class Repository {
-  constructor() {
-    this.product = JSON.parse(document.getElementById('data').textContent);
+  productData: Product;
+
+  constructor(private http: HttpClient) {
+    this.getProduct(1);
   }
 
-  product: Product;
+  getProduct(id: number) {
+    this.http.get<Product>('/api/products/' + id).subscribe((p) => {
+      this.productData = p;
+    });
+  }
+
+  get product(): Product {
+    return this.productData;
+  }
 }
