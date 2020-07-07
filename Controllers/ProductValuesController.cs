8327ce4dce6431d325a52e627bb233c3fe65b6a3
isidoreduc <isidoreducasse79@gmail.com>
2020-07-07 11:36:40 +0200

    get product web service

---
+++ Controllers/ProductValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.Controllers
+{
+    [Route("api/product")]
+    public class ProductValuesController : Controller
+    {
+        private readonly DataContext _ctx;
+
+        public ProductValuesController(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Product> GetProduct(long id) => 
+            await _ctx.Products.FindAsync(id);
+            // await _ctx.Products.FindAsync(id);
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
