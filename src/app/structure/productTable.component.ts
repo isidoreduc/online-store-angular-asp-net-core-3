e817402eedcbf888ca57eb07197c8df0d7678526
isidoreduc <isidoreducasse79@gmail.com>
2020-07-12 09:22:46 +0200

    routing by url - to details

--- src/app/structure/productTable.component.ts
+++ src/app/structure/productTable.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Repository } from '../models/repository';
 import { Product } from '../models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'product-table',
   templateUrl: './productTable.component.html',
 })
 export class ProductTableComponent {
-  constructor(private repo: Repository) {}
+  constructor(private repo: Repository, private router: Router) {}
 
   get products(): Product[] {
     return this.repo.products;
@@ -15,5 +16,6 @@ export class ProductTableComponent {
 
   selectProduct(id: number) {
     this.repo.getProduct(id);
+    this.router.navigateByUrl("/detail");
   }
 }
