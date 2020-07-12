f6204b7bce8b420b9e374c5f57b94a2bfe8a3de4
isidoreduc <isidoreducasse79@gmail.com>
2020-07-12 09:42:54 +0200

    direct resource navigation

--- src/app/structure/productDetail.component.ts
+++ src/app/structure/productDetail.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Repository } from '../models/repository';
 import { Product } from '../models/product.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'product-detail',
   templateUrl: 'productDetail.component.html',
 })
 export class ProductDetailComponent implements OnInit {
-  constructor(private repo: Repository) {}
-
+  constructor(
+    private repo: Repository,
+    router: Router,
+    activeRoute: ActivatedRoute
+  ) {
+    let id = Number.parseInt(activeRoute.snapshot.params['id']);
+    if (id) {
+      this.repo.getProduct(id);
+    } else {
+      router.navigateByUrl('/');
+    }
+  }
   ngOnInit() {}
 
   get product(): Product {
