dde894c87902ab4a9f894d998d6a4bb25d062ec8
isidoreduc <isidoreducasse79@gmail.com>
2020-07-14 17:34:22 +0200

    show product list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductTableComponent } from './structure/productTable.component';
 import { ProductDetailComponent } from './structure/productDetail.component';
+import { StoreRootComponent } from './store/storeRoot.component';
 
 const routes: Routes = [
-  { path: 'table', component: ProductTableComponent },
-  { path: 'detail', component: ProductDetailComponent },
-  { path: 'detail/:id', component: ProductDetailComponent },
-  { path: '', component: ProductTableComponent },
+  // { path: 'table', component: ProductTableComponent },
+  // { path: 'detail', component: ProductDetailComponent },
+  // { path: 'detail/:id', component: ProductDetailComponent },
+  // { path: '', component: ProductTableComponent },
+  { path: 'store', component: StoreRootComponent },
+  { path: '', redirectTo: '/store', pathMatch: 'full' },
 ];
 
 @NgModule({
