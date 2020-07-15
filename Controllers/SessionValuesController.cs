cb82d89c0376ba1a1195ff49dc89d95363cab39a
isidoreduc <isidoreducasse79@gmail.com>
2020-07-15 11:16:25 +0200

    session controller

---
+++ Controllers/SessionValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ServerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.Controllers
+{
+    
+    [Route("/api/session")]
+    [ApiController]
+    public class SessionValuesController : Controller
+    {
+        [HttpGet("cart")]
+        public IActionResult GetCart()
+        {
+            return Ok(HttpContext.Session.GetString("cart"));
+        }
+        [HttpPost("cart")]
+        public void StoreCart([FromBody] CartProductSelection[] products)
+        {
+            var jsonData = JsonConvert.SerializeObject(products);
+            HttpContext.Session.SetString("cart", jsonData);
+        }
+    }
+
+    
+}
