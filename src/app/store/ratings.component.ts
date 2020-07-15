31279cc1590a5b438eb62788179fe19f05cfe696
isidoreduc <isidoreducasse79@gmail.com>
2020-07-15 10:37:29 +0200

    star rating

--- src/app/store/ratings.component.ts
+++ src/app/store/ratings.component.ts
@@ -1,11 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'store-ratings',
   templateUrl: 'ratings.component.html',
 })
 export class RatingsComponent implements OnInit {
-  constructor() {}
+  constructor() { }
+  
+  @Input()
+    product: Product;
+
+    get stars(): boolean[] {
+        if (this.product != null && this.product.ratings != null) {
+            let total = this.product.ratings.map(r => r.stars)
+                .reduce((prev, curr) => prev + curr, 0);
+            let count = Math.round(total / this.product.ratings.length);
+            return Array(5).fill(false).map((value, index) => {
+                return index < count;
+            });
+        } else {
+            return [];
+        }
+    }
+
 
   ngOnInit() {}
 }
